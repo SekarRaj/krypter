9a8d1e2057759c349794a7061eb3edd1bd9240c0
Rajasekaran Rajaram <sekar_raj@hotmail.com>
2017-05-09 00:36:55 +0530

    Added Respone and Path values

--- src/main/java/com/krypter/EmployeeCrudService.java
+++ src/main/java/com/krypter/EmployeeCrudService.java
@@ -2,14 +2,24 @@ package com.krypter;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/employee")
 public class EmployeeCrudService {
 	@GET
+	@Path("/{name}")
 	@Produces({ MediaType.TEXT_HTML })
-	public String sayHello() {
-		return "Hello world!!!";
+	public Response sayDude(@PathParam("name") final String name) {
+		return Response.status(200).entity("You sent " + name).build();
+	}
+
+	@GET
+	@Path("/")
+	@Produces({ MediaType.TEXT_HTML })
+	public Response sayHello() {
+		return Response.status(200).entity("This is just a string").build();
 	}
 }
