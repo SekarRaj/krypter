ffa8b39773483c5d9c74a4fa4f8f6d5b7398044a
Rajasekaran Rajaram <sekar_raj@hotmail.com>
2017-05-14 04:29:42 +0530

    Merged into single project

---
+++ src/main/java/com/krypter/domain/Employee.java
@@ -0,0 +1,73 @@
+package com.krypter.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({ @NamedQuery(name = "Employee.getAllEmployees", query = "Select e from Employee e") })
+@Table(name = "TBL_EMP_MST")
+public class Employee {
+	@Id
+	@Column(name = "id")
+	private String id;
+
+	@Column(name = "firstName")
+	private String firstName;
+
+	@Column(name = "lastName")
+	private String lastName;
+
+	@Column(name = "dept")
+	private String department;
+
+	public Employee() {
+	}
+
+	public Employee(String id, String firstName, String lastName, String department) {
+		this.setId(id);
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setDepartment(department);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return this.getId() + ", " + this.getFirstName() + ", " + this.getLastName() + ", " + this.getDepartment();
+	}
+}
---
+++ src/main/java/com/krypter/repository/EmployeeRepository.java
@@ -0,0 +1,73 @@
+package com.krypter.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.krypter.dal.DataAccessLayer;
+import com.krypter.domain.Employee;
+import com.krypter.identifiers.EmployeeIdentifier;
+import com.krypter.identifiers.IdentifierGenerator;
+
+public class EmployeeRepository {
+	private static volatile EmployeeRepository INSTANCE;
+	private static Object lock = new Object();
+
+	// @Inject
+	private IdentifierGenerator idGenerator;
+
+	private EntityManager em;
+
+	private EmployeeRepository() {
+		idGenerator = new EmployeeIdentifier();
+		em = DataAccessLayer.getEntityManager();
+	}
+
+	public static EmployeeRepository getInstance() {
+		if (INSTANCE == null) {
+			synchronized (lock) {
+				if (INSTANCE == null)
+					INSTANCE = new EmployeeRepository();
+			}
+		}
+		return INSTANCE;
+	}
+
+	public String createEmployee(String firstName, String lastName, String dob) {
+		em.getTransaction().begin();
+		String id = idGenerator.generateId();
+		Employee emp = new Employee(id, firstName, lastName, dob);
+		em.persist(emp);
+		em.getTransaction().commit();
+		return emp.getId();
+	}
+
+	public boolean editEmployee(Employee emp) {
+		em.getTransaction().begin();
+		Employee employee = em.find(Employee.class, emp.getId());
+		employee.setFirstName(emp.getFirstName());
+		employee.setLastName(emp.getLastName());
+		employee.setDepartment(emp.getDepartment());
+		em.getTransaction().commit();
+		return true;
+	}
+
+	public boolean deleteEmployee(String empId) {
+		em.getTransaction().begin();
+		Employee employee = em.find(Employee.class, empId);
+		em.remove(employee);
+		em.getTransaction().commit();
+		return true;
+	}
+
+	public Employee getEmployee(String empId) {
+		Employee emp = em.find(Employee.class, empId);
+		return emp;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Employee> getEmployees() {
+//		return em.createQuery("Employee.getAllEmployees", Employee.class).getResultList();
+		return em.createQuery("from Employee").getResultList();
+	}
+}
